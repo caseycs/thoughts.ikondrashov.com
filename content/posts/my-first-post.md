6ae475d3e24013018de2e32585afb417b3d285f4
Ilia Kondrashov <caseycs@gmail.com>
2017-12-16 18:07:13 +0100

    copy

--- content/posts/my-first-post.md
+++
@@ -1,7 +0,0 @@
----
-title: "My First Post"
-date: 2017-12-16T13:11:57+01:00
-draft: true
----
-
-my first post 
--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -6,15 +6,6 @@ then
 #    exit 1;
 fi
 
-echo "Deleting old publication"
-rm -rf docs
-mkdir docs
-git worktree prune
-rm -rf .git/worktrees/docs/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages docs origin/gh-pages
-
 echo "Removing existing files"
 rm -rf docs/*
 
@@ -22,4 +13,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd docs && git add --all && git commit -m "Publishing to gh-pages (publish_to_ghpages.sh)" && git push -f origin gh-pages
+cd docs && git add --all && git commit -m "Publishing to gh-pages (publish_to_ghpages.sh)" && git push origin gh-pages
